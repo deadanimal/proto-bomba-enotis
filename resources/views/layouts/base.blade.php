f4d91f4de8bd2f75c7b50eb4eabe0b78b6fc4abc
WanHuzairipipe <wan.huzairi@pipeline.com.my>
2021-07-29 16:42:49 +0800

    text sidebar update

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -117,7 +117,7 @@
             <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
             <a class="navbar-brand m-0" href="https://demos.creative-tim.com/soft-ui-dashboard-pro/pages/dashboards/default.html" target="_blank">
                 <img src="../../assets/img/bomba-logo.png" class="navbar-brand-img h-100" alt="main_logo">
-                <span class="ms-1 font-weight-bold">Bomba - eNotis</span>
+                <span class="ms-1 font-weight-bold">eNotis (Menghapus Bahaya Kebakaran)</span>
             </a>
         </div>
         <hr class="horizontal dark mt-0">
